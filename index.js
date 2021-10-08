07b669beca7cc0bf4814979419045d3903be9a41
nzmzrw <91615947+nzmzrw@users.noreply.github.com>
2021-10-08 23:18:40 +0900

    環境変数設定を追加

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express')
+  const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
@@ -7,4 +7,15 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/times', (req, res) => res.send(countHitsuji()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+  countHitsuji = () => {
+    let result = ''
+    const times = process.env.TIMES || 5
+    for (i = 0; i < times; i++) {
+      result += i + ' '
+    }
+    return result;
+  }
