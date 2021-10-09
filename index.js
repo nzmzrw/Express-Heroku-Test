992099cf6daeca46a281f69899424ca6c78ad136
nzmzrw <91615947+nzmzrw@users.noreply.github.com>
2021-10-09 14:02:44 +0900

    .envファイルを追跡から除外

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-  const express = require('express')
+const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const { Pool } = require('pg');
 
 express()
   .use(express.static('public'))
@@ -8,6 +9,18 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/times', (req, res) => res.send(countHitsuji()))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM member');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
@@ -19,3 +32,8 @@ express()
     }
     return result;
   }
+
+  const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+  });
