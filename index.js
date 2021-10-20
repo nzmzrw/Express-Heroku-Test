2182e394c91521319f9487440a81850633f63c6e
nzmzrw <91615947+nzmzrw@users.noreply.github.com>
2021-10-20 21:58:15 +0900

    テスト

--- index.js
+++ index.js
@@ -3,30 +3,10 @@ const PORT = process.env.PORT || 5000
 const { Pool } = require('pg');
 
 express()
-  .use(express.static('public'))
   .set('views', require('path').join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/test', (req, res) => {
-    const client = pool.connect()
-      .then(() => { console.log("Connected successfuly"); })
-      .catch(() => { console.log("Error"); })
-    client.release();
-  })
+  .get('/test', readTopPage)
 
-  // .get('/db', async (req, res) => {
-  //   try {
-  //     console.log("Connected successfuly");
-  //     const client = await pool.connect()
-  //     const result = await client.query('SELECT * FROM member');
-  //     const results = { 'results': (result) ? result.rows : null};
-  //     res.render('pages/db', results );
-
-  //     client.release(); 
-  //   } catch (err) {
-  //     console.error(err);
-  //     res.send("Error " + err);
-  //   }
-  // })
   .listen(PORT, () => console.log(`Listening on ${PORT}`))
 
 //環境変数より、接続文字列を取得
@@ -34,3 +14,11 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
+
+//TOP画面読み込み
+function readTopPage(req, res, next) {
+  const client = pool.connect()
+    .then(() => { console.log("Connected successfuly"); })
+    .catch(() => { console.log("Error"); })
+  res.render("pages/index.ejs");
+}
