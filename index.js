a26f545b4a6bd288ed948567c947419585db4bdd
nzmzrw <91615947+nzmzrw@users.noreply.github.com>
2022-02-06 01:38:17 +0900

    commit

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const PORT = process.env.PORT || 5000
 const { Pool } = require('pg');
 
+const a_filename = "AccessCounter.txt" // Access数記録ファイル名
+let Access = 0; //アクセス数
+
+App_Start();
+process.on("SIGINT", App_PreEnd); // Ctrl + C 用
+process.on('exit', App_End); // プロセス終了
+
 express()
   .set('views', require('path').join(__dirname, 'views'))
   .set('view engine', 'ejs')
@@ -15,31 +22,57 @@ const pool = new Pool({
   ssl: { rejectUnauthorized: false }
 });
 pool.connect()
-  .then(console.log("Client disconnected successfully"));
-
-
-  // アクセス数
-  let test = 0;
+  .then(console.log("Database Connected."));
 
 //TOP画面読み込み
 async function readTopPage(req, res, next) {
   try {
     //テーブル取得
     const result = await pool.query('select * from ramen')
-    console.table(result.rows)
 
     //取得したデータで、テーブルを生成
     let items = [];
     items = result.rows;
 
+    // アクセス数記録
+    Access++;
+    console.log("Access Count : " + Access.toString());
+
+    //ページ表示
     res.render("pages/main.ejs", {
       items: items,
+      Access: Access,
     });
-
-    test++;
-    console.log("Access Count ; " + test);
   }
   catch (ex) {
     console.log(`Something wrong happend ${ex}`)
   }
+}
+
+function App_Start() {
+  //アクセス数ファイル読み込み
+  const fs = require("fs");
+  if (fs.existsSync(a_filename)) {
+    fs.readFile(a_filename, "utf-8", (err, fd) => {
+      if (err) {
+        console.log("1");
+        throw err;
+      }
+      Access = fd.toString();
+      console.log("Access Count : " + Access.toString() + " (Init)");
+    });
+  }
+}
+
+function App_PreEnd() {
+  process.exit(0);
+}
+
+function App_End() {
+  var fs = require('fs');
+  var text = Access.toString();
+  fs.writeFileSync(a_filename, text);
+
+  pool.end();
+  console.log("Process End.");
 }
